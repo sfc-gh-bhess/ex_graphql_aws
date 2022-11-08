2dd3f16b338f3dcdf444f263b3f17d1a97a9cd7c
brian.hess <brian.hess@snowflake.com>
2022-11-08 16:39:39 -0500

    error handling

--- snow_graphql/snow_procs.py
+++ snow_graphql/snow_procs.py
@@ -1,35 +1,64 @@
 from snowflake.snowpark.functions import col
 import snowflake.snowpark.functions as f
 import os
+import logging
+import datetime
+
+# Set Logging Level
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 table = os.getenv('SNOW_TABLE')
 
 def busy_airports(session, begin, end, deparr, nrows):
     df = session.table(table)
     if begin and end:
-        df = df.filter((col('FLIGHT_DATE') >= begin) & (col('FLIGHT_DATE') <= end))
+        try:
+            d_begin = datetime.date.fromisoformat(begin)
+            d_end = datetime.date.fromisoformat(end)
+            df = df.filter((col('FLIGHT_DATE') >= d_begin) & (col('FLIGHT_DATE') <= d_end))
+        except ValueError as ex:
+            logger.error('Bad dates provided: ' + str(ex))
+            raise ValueError("Error: Bad dates provided")
     deparr = deparr if deparr == 'ARRAPT' else 'DEPAPT'
     try:
-        nrows = int(nrows)
-    except:
-        nrows = 20
+        nrows = int(nrows or 20)
+    except ValueError as ex:
+        logger.error('nrows must be an integer')
+        raise ValueError('nrows must be an integer')
     df = df.with_column_renamed(col(deparr), 'apt').group_by(col('apt')) \
                     .agg(f.count('apt').alias('ct')) \
                     .sort(col('ct').desc()) \
                     .limit(nrows) 
-    return [x.as_dict() for x in df.to_local_iterator()]
+    try:
+        retval = [x.as_dict() for x in df.to_local_iterator()]
+    except Exception as ex:
+        logger.error('Failed to retrieve data frame: ' + str(ex))
+        raise
+    return retval
 
 def airport_daily(session, apt, begin, end):
     df = session.table(table)
     if begin and end:
-        df = df.filter((col('FLIGHT_DATE') >= begin) & (col('FLIGHT_DATE') <= end))
+        try:
+            d_begin = datetime.date.fromisoformat(begin)
+            d_end = datetime.date.fromisoformat(end)
+            df = df.filter((col('FLIGHT_DATE') >= d_begin) & (col('FLIGHT_DATE') <= d_end))
+        except ValueError as ex:
+            logger.error('Bad dates provided: ' + str(ex))
+            raise ValueError("Error: Bad dates provided")
     df = df.group_by(col('FLIGHT_DATE')) \
         .agg([ \
                 f.sum(f.when(col('DEPAPT') == apt, f.lit(1)).otherwise(f.lit(0))).alias('depct'), \
                 f.sum(f.when(col('ARRAPT') == apt, f.lit(1)).otherwise(f.lit(0))).alias('arrct') \
             ]) \
         .sort(col('FLIGHT_DATE').asc())
-    return [x.as_dict() for x in df.to_local_iterator()]
+    try:
+        retval = [x.as_dict() for x in df.to_local_iterator()]
+    except Exception as ex:
+        logger.error('Failed to retrieve data frame: ' + str(ex))
+        raise
+    return retval
 
 airline_list = {
         'AA':'American', 
@@ -42,12 +71,23 @@ airline_list = {
 def airport_daily_carriers(session, apt, begin, end, deparr):
     df = session.table(table)
     if begin and end:
-        df = df.filter((col('FLIGHT_DATE') >= begin) & (col('FLIGHT_DATE') <= end))
+        try:
+            d_begin = datetime.date.fromisoformat(begin)
+            d_end = datetime.date.fromisoformat(end)
+            df = df.filter((col('FLIGHT_DATE') >= d_begin) & (col('FLIGHT_DATE') <= d_end))
+        except ValueError as ex:
+            logger.error('Bad dates provided: ' + str(ex))
+            raise ValueError("Error: Bad dates provided")
     deparr = deparr if deparr == 'ARRAPT' else 'DEPAPT'
     df = df.filter(col('CARRIER').isin(list(airline_list.keys()))) \
         .filter(col(deparr) == apt) \
         .group_by([col('FLIGHT_DATE'), col('CARRIER')]) \
         .agg(f.count('FLIGHT_DATE').alias('ct')) \
         .sort(col('FLIGHT_DATE').asc())
-    return [x.as_dict() for x in df.to_local_iterator()]
+    try:
+        retval = [x.as_dict() for x in df.to_local_iterator()]
+    except Exception as ex:
+        logger.error('Failed to retrieve data frame: ' + str(ex))
+        raise
+    return retval
 
